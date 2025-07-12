f8aa7bd54f44a6183b0879183f4b163740f7c8eb
Sabith <masterdevsabith@gmail.com>
2025-07-12 23:01:18 +0530

    set up reactbit.devs

--- app/components/includes/Footer.tsx
+++ app/components/includes/Footer.tsx
@@ -1,3 +1,14 @@
+import CircularText from "../ui/CircularText";
+
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <CircularText
+        text="HIRE*ME*HIRE*ME*HIRE*ME*"
+        onHover="speedUp"
+        spinDuration={20}
+        className="custom-class text-black"
+      />
+    </footer>
+  );
 }
