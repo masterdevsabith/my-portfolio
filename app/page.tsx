805966a44fba880236ef6d64e107a5e5119631bf
Sabith <masterdevsabith@gmail.com>
2025-07-12 22:37:49 +0530

    footer section file added

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import About from "./components/About";
 import ContentCreation from "./components/ContentCreation";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
+import Footer from "./components/includes/Footer";
+import FadeIn from "./components/ui/fadein-wrapper";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -18,11 +20,18 @@ export default function Home() {
   return (
     <main className={`relative bg-[var(--background)] ${poppins.className}`}>
       <Navbar />
+
       <Hero />
+
       <About />
+
       <ContentCreation />
+
       <Works />
+
       <Contact />
+
+      <Footer />
     </main>
   );
 }
